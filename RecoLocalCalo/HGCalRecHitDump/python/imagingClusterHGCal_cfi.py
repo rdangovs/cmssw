2cff2c781359b2058fb8aa084d3cbbae792121aa
lgray <lindsey.gray@gmail.com>
2016-06-21 06:33:04 -0500

    new parameters for clustering

--- RecoLocalCalo/HGCalRecHitDump/python/imagingClusterHGCal_cfi.py
+++ RecoLocalCalo/HGCalRecHitDump/python/imagingClusterHGCal_cfi.py
@@ -8,8 +8,8 @@ imagingClusterHGCal =  cms.EDProducer(
     detector = cms.string("EE"),
     doSharing = cms.bool(False),
     deltac = cms.double(2.),
-    ecut = cms.double(0.0001),
-    kappa = cms.double(10.),
+    ecut = cms.double(0.06),
+    kappa = cms.double(12.75),
     verbosity = cms.untracked.uint32(3)
     )
 
